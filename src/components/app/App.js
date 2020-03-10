9c0ca531585ab7f536cb2069d21ebf6d6324e7b0
Ekaterina <sedpost1@mail.ru>
2020-03-10 20:17:53 +0500

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './App.module.sass';
+
+function App() {
+  return (
+    <div className={s.container}>
+      6666666666666666
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/article/Article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Article.module.sass';
+
+function Article() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Article;
