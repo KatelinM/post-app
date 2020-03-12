7556ea7aa4da1ca44b43c1f0c63f05430eecf8d8
Ekaterina <sedpost1@mail.ru>
2020-03-12 17:47:45 +0500

    +кнопка показать больше комментариев

--- src/components/comment-box/CommentBox.js
+++ src/components/comment-box/CommentBox.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import s from './CommentBox.sass';
 import Comment from "../comment/Comment";
 import timeSince from "../helpers/timeSince";
 
-const CommentBox = ( {comments} ) => {
-    let getComment = (commentList) => {
-         return (
-            commentList
-                .map(c => {
-                    console.log(c)
-                    return (
-                        <div key={c.id} className="comment-group">
-                            <Comment
-                                id={c.id}
-                                author={c.author}
-                                time={timeSince(c.date)}
-                                rating={c.rating}
-                                message={c.message}/>
-                            { c.comments && c.comments.length ? getComment(c.comments) : null }
-                        </div>
-                    )
-                }
-             )
-         )
-    };
+let CommentsGroup = ({comment}) => {
+    let [showMore, toggleShowMore] = useState(false)
 
-    let commentsElements = getComment(comments);
+    const hasChildComments = comment.comments && comment.comments.length;
+
+    return(
+        <>
+            <div className={ hasChildComments ? 'group-comments' : 'single-comment' }>
+                <div className="head-comment">
+                    <Comment {...comment} time={timeSince(comment.date)} />
+                </div>
+                {hasChildComments && <>
+                    <div className={`sub-comments ${!showMore ? 'hide' : null}`} >
+                        { comment.comments.map(c => {
+                            return <Comment {...c} time={timeSince(c.date)} />
+                        }) }
+
+                        <button className ='button-more' onClick={ ()=>toggleShowMore(!showMore) }>
+                            { showMore ? 'Свернуть' : 'Показать еще' }
+                        </button>
+                    </div>
+                </>
+                }
+            </div>
+        </>
+    )
+};
+
+const CommentBox = ( {comments} ) => {
     return (
         <div className="comment-box">
             <div className="title">Комментарии:</div>
-            { commentsElements }
+
+            { comments.map(c => {
+                return <CommentsGroup comment={c}/>
+            }) }
+
         </div>
     );
 };
