70aa04ae4a1bf99504f8490b984bda5f15e3c4f3
Ekaterina <sedpost1@mail.ru>
2020-03-14 13:08:00 +0500

    build v

---
+++ build1/precache-manifest.92a3ff2ae2fd918a64c5c8fd1812abc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4618692f6db2b347a915a69585c82f45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd2d785215b5e2d5d51",
+    "url": "/static/css/main.f0863711.chunk.css"
+  },
+  {
+    "revision": "fcb4f84d4ca593485986",
+    "url": "/static/js/2.e48ff16e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.e48ff16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd2d785215b5e2d5d51",
+    "url": "/static/js/main.7ed0f33c.chunk.js"
+  },
+  {
+    "revision": "4ba75d14d87c58cd6169",
+    "url": "/static/js/runtime-main.13a4e91b.js"
+  }
+]);
